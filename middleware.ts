990dcf4a7e5da76ca7c31f348f699d9c5505ffec
xukev22 <119141959+xukev22@users.noreply.github.com>
2025-07-10 14:26:26 -0400

    protect all routes (except home)

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
-// Without a defined matcher, this one line applies next-auth
-// to the entire project
 export { default } from "next-auth/middleware";
 
-// Applies next-auth only to matching routes - can be regex
-// Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ["/dashboard/:path*"], // Ensures all routes under /dashboard are protected
+  matcher: [
+    /*
+      Match all paths EXCEPT:
+      - the homepage (/)
+      - static files (/_next, /images, etc.)
+      - API routes (/api)
+    */
+    "/((?!api|_next|.*\\..*|$).*)",
+  ],
 };
